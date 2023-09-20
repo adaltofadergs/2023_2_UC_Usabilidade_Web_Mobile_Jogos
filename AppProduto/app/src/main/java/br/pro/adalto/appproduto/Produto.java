46651eaededf175afffe1ce072a4c30555932a1d
Adalto Selau Sparremberger <adalto.sparremberger@animaeducacao.com.br>
2023-09-19 21:44:29 -0300

    Desenvolvendo o AppProduto - Classe Produto

---
+++ AppProduto/app/src/main/java/br/pro/adalto/appproduto/Produto.java
@@ -0,0 +1,23 @@
+package br.pro.adalto.appproduto;
+
+public class Produto {
+
+    public int id;
+    public String nome;
+    public double preco;
+
+    public Produto() {
+
+    }
+
+    public Produto(int id, String nome, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+}
